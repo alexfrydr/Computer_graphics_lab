01ab3abb91f47a5a854a6a4bad68a235853de9c5
Alex <63281155+alexfrydr@users.noreply.github.com>
2025-12-19 15:40:10 +0800

    Update Lab1.cpp

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,4 +1,5 @@
-﻿#include <cstdio>
+#include <cstdio>
+// Определение макросов для динамической загрузки библиотек GLEW и GLFW
 #define GLEW_DLL
 #define GLFW_DLL
 
@@ -6,6 +7,7 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+    // Инициализация библиотеки GLFW. Если инициализация не удалась, выводим ошибку и завершаем программу
     if (!glfwInit()) {
         fprintf(stderr, "ERROR: could not start GLFW3.\n");
         return 1;
@@ -21,6 +23,7 @@ int main() {
         return -1;
     }
 
+    // Установка текущего контекста OpenGL для этого окна
     glfwMakeContextCurrent(window);
     glewExperimental = GL_TRUE;
 
@@ -30,34 +33,44 @@ int main() {
         return 1;
     }
 
+    // Получение и вывод информации о версии GLEW, OpenGL и устройстве рендеринга
     const GLubyte* version_str = glGetString(GL_VERSION);
     const GLubyte* device_str = glGetString(GL_RENDERER);
-
     printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
     printf("This version OpenGL running is %s\n", version_str);
     printf("This device OpenGL running is %s\n", device_str);
 
     while (!glfwWindowShouldClose(window)) {
+        // Установка цвета очистки буфера (чёрный цвет с полной непрозрачностью)
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // Очистка буфера цвета
         glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_QUADS);
+
+        // Установка цвета для первой и второй вершин (фиолетовый оттенок)
         glColor3f(0.6f, 0.2f, 0.9f);
-        glVertex2f(-0.5f, 0.5f);
+        glVertex2f(-0.5f, 0.5f); // Верхняя левая вершина
 
         glColor3f(0.6f, 0.2f, 0.9f);
-        glVertex2f(0.5f, 0.5f);
+        glVertex2f(0.5f, 0.5f); // Верхняя правая вершина
+
+        // Установка цвета для третьей и четвёртой вершин (серый оттенок)
+        glColor3f(0.3f, 0.3f, 0.3f);
+        glVertex2f(0.5f, -0.5f); // Нижняя правая вершина
 
         glColor3f(0.3f, 0.3f, 0.3f);
-        glVertex2f(0.5f, -0.5f);
-
-        glColor3f(0.3f, 0.3f, 0.3f);
-        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(-0.5f, -0.5f); // Нижняя левая вершина
+        
+        // Завершение рисования
         glEnd();
 
+        // Обмен буферами (double buffering) для отображения нарисованного кадра
         glfwSwapBuffers(window);
+
+        // Обработка событий (ввод с клавиатуры, мыши и т.д.)
         glfwPollEvents();
     }
-
+    // Завершение работы GLFW
     glfwTerminate();
 
 }
