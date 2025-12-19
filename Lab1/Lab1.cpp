b76e9376bafc8bb56705698d5a18ce6359c1f5ad
Alex <63281155+alexfrydr@users.noreply.github.com>
2025-12-19 13:29:02 +0800

    Add files via upload

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,63 @@
+﻿#include <cstdio>
+#define GLEW_DLL
+#define GLFW_DLL
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3.\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    GLFWwindow* window = glfwCreateWindow(512, 512, "Прямоугольник - Вариант 28", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+
+    GLenum ret = glewInit();
+    if (GLEW_OK != ret) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(ret));
+        return 1;
+    }
+
+    const GLubyte* version_str = glGetString(GL_VERSION);
+    const GLubyte* device_str = glGetString(GL_RENDERER);
+
+    printf("Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    printf("This version OpenGL running is %s\n", version_str);
+    printf("This device OpenGL running is %s\n", device_str);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_QUADS);
+        glColor3f(0.6f, 0.2f, 0.9f);
+        glVertex2f(-0.5f, 0.5f);
+
+        glColor3f(0.6f, 0.2f, 0.9f);
+        glVertex2f(0.5f, 0.5f);
+
+        glColor3f(0.3f, 0.3f, 0.3f);
+        glVertex2f(0.5f, -0.5f);
+
+        glColor3f(0.3f, 0.3f, 0.3f);
+        glVertex2f(-0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+}
